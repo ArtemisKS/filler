e6bbea1afb05a11b5163b07508f94109d2559fb3
ArtemisKS <Artemkupriyanets@yahoo.com>
2019-03-26 21:58:39 +0200

    finished visual, albeit could be improved

--- filler_vis.py
+++ filler_vis.py
@@ -6,23 +6,25 @@ import time
 map_size_x, map_size_y = 0, 0
 piece_size_x, piece_size_y = 0, 0
 iter_ind = 0
-PLAYER_1 = 'X'
-PLAYER_2 = 'O'
-player = ''
+PLAYER_1 = 'O'
+PLAYER_2 = 'X'
+player = PLAYER_1
 init_round = True
-player_assigned = False
 BOX_SIZE = 20
-Y_INDENT = 200
-INIT_X = Y_INDENT + 20
-INIT_Y = Y_INDENT + 20
+Y_INDENT = 100
+INIT_X = Y_INDENT
+INIT_Y = Y_INDENT
 SCR_WIDTH = 1200
 SCR_HEIGHT = 1200
-LINE_WIDTH = 2
-player1_points = 1
-player2_points = 1
+LINE_WIDTH = 1
+player1_name = ''
+player2_name = ''
+RED = gr.color_rgb(200, 0, 0)
+BLUE = gr.color_rgb(0, 0, 200)
 
 
-pl_map = []
+# pl_map = []
+arena = None
 piece = []
 init_coords = {'X' : [None, None], 'O': [None, None]}
 
@@ -33,10 +35,13 @@ def error_and_quit(mes):
 	print('Error:', mes)
 	quit()
 
-def make_rect(x1, y1, x2, y2):
-	return gr.Rectangle(gr.Point(x1, y1), gr.Point(x2, y2))
+def make_rect(x1, y1, x2, y2, color, line_width):
+	rect = gr.Rectangle(gr.Point(x1, y1), gr.Point(x2, y2))
+	rect.setOutline(color)
+	rect.setWidth(line_width)
+	return rect
 
-def parse_piece(stdin):
+def parse_piece(stdin, piece_size_y):
 	i = 0
 	# print(f'piece_size_y: {piece_size_y}')
 	for line in stdin:
@@ -50,86 +55,9 @@ def get_coords(spl):
 	str1, str2 = spl[-1], spl[-2]
 	str2 = str2[1:-1]
 	str1 = str1[:-1]
-	if not re.match("^[0-9]*$", str1) or not re.match("^[0-9]*$", str2):
+	if not re.match("^-?[0-9]*$", str1) or not re.match("^-?[0-9]*$", str2):
 		error_and_quit('wrong coords')
 	return [int(str2), int(str1)]
-
-def	parse_init():
-	global player, map_size_x, map_size_y, player_assigned
-	for line in sys.stdin:
-		spl = line[:-1].split(' ')
-		if 'p1' in spl and not player_assigned:
-			player = PLAYER_1
-			player_assigned = True
-			continue
-		elif 'p2' in spl and not player_assigned:
-			player = PLAYER_2
-			player_assigned = True
-			continue
-		elif 'Plateau' in spl:
-			map_size_y = int(spl[1])
-			map_size_x = int(spl[2][:-1])
-			arena = [[gr.Rectangle(gr.Point(0, 0), gr.Point(0, 0))] * map_size_x for i in range(map_size_y)]
-			return arena
-		elif 'exec' in spl or 'launched' in spl:
-			continue
-		elif line[0] == '#':
-			continue
-		else:
-			error_and_quit(f'Something\'s wrong in your initial input, guys. Here\'s the faulty line: {line}')
-		i += 1
-	return None
-	
-if init_round:
-	arena = parse_init()
-
-def parse_map_and_display():
-	global init_round, map_size_x, map_size_y, piece_size_x, piece_size_y, player
-	arena_ind = 0
-	# not_done_yet = True
-	for line in sys.stdin:
-		spl = line[:-1].split(' ')
-		# print(f'spl: {spl}, i: {i}')
-		if 'Plateau' in spl:
-			if int(spl[1]) != map_size_y or int(spl[2][:-1]) != map_size_x:
-				error_and_quit(f'new map_size({int(spl[1])}, {int(spl[2][:-1])}) is not equal to initial ({map_size_y}, {map_size_x})')
-			continue
-		# if not init_round and not_done_yet:
-		# 	i = 0
-		# 	for line in sys.stdin:
-		# 		if i >= map_size_x:
-		# 			break
-		# 		i += 1
-		# 	not_done_yet = False
-		elif re.match('^ +[0-9]+$', line):
-			continue
-		elif re.match(f'^[0-9]+ .{{{map_size_x}}}$', line):
-			ar_line = spl[1]
-			if init_round and ('X' in ar_line or 'O' in ar_line):
-				key = 'X' if 'X' in ar_line else 'O'
-				init_coords[key][0] = arena_ind
-				init_coords[key][1] = ar_line.index(key)
-			pl_map.append(ar_line)
-			arena_ind += 1
-			continue
-		elif 'Piece' in spl:
-			piece_size_y = int(spl[1])
-			piece_size_x = int(spl[2][:-1])
-			parse_piece(sys.stdin)
-			continue
-		elif '<got' in spl:
-			coords = get_coords(spl)
-			display(coords, piece)
-			pl_map.clear()
-			piece.clear()
-			player = PLAYER_1 if player == PLAYER_2 else PLAYER_2
-			# not_done_yet = True
-			# time.sleep(3)
-			arena_ind = 0
-			continue
-		else:
-			error_and_quit(f'Something\'s wrong in your input, guys. Here\'s the faulty line: {line}')
-	return
 
 def det_item_indices(item, x_pos):
 	st_ind = item.index('*') + x_pos
@@ -139,6 +67,36 @@ def det_item_indices(item, x_pos):
 			return st_ind, end_ind
 	return st_ind, None
 
+def draw_line(x1, y1, x2, y2, color):
+	line = gr.Line(gr.Point(x1, y1), gr.Point(x2, y2))
+	line.setOutline(color)
+	line.setWidth(LINE_WIDTH)
+	line.draw(win)
+	return line
+
+def draw_lines(color):
+	for i in range(map_size_y + 1):
+		cur_y = INIT_Y + i * BOX_SIZE
+		line = draw_line(INIT_X, cur_y, INIT_X + (map_size_x - 1) * BOX_SIZE, cur_y, color)
+	for i in range(map_size_x):
+		cur_x = INIT_X + i * BOX_SIZE
+		line = draw_line(cur_x, INIT_Y, cur_x, INIT_Y + map_size_y * BOX_SIZE, color)
+
+def draw_start_label(starter):
+	text = gr.Text(gr.Point(win.getWidth()/2 - 25, SCR_HEIGHT - INIT_Y/2), 'Click on the screen to start')
+	text.setTextColor(RED if starter == PLAYER_1 else BLUE)
+	text.setSize(25)
+	text.setStyle('italic')
+	text.draw(win)
+	return text
+
+def draw_quit_label(winner):
+	text = gr.Text(gr.Point(win.getWidth()/2 - 25, SCR_HEIGHT - INIT_Y/2), 'Click on the screen to quit')
+	text.setTextColor(RED if winner == player1_name else BLUE)
+	text.setSize(25)
+	text.setStyle('italic')
+	text.draw(win)
+
 def draw_arena():
 	global init_round
 	init_round = False
@@ -147,13 +105,12 @@ def draw_arena():
 	for i in range(map_size_y):
 		cur_x = INIT_X
 		for j in range(map_size_x - 1):
-			rect = make_rect(cur_x, cur_y, cur_x + BOX_SIZE, cur_y + BOX_SIZE)
-			rect.setOutline(gr.color_rgb(0, 0, 0))
-			rect.setWidth(LINE_WIDTH)
+			rect = make_rect(cur_x, cur_y, cur_x + BOX_SIZE, cur_y + BOX_SIZE, gr.color_rgb(0, 0, 0), LINE_WIDTH)
 			arena[i][j] = rect
 			cur_x += BOX_SIZE
 		cur_y += BOX_SIZE
 	print(f'init_coords: {init_coords}')
+	draw_lines(gr.color_rgb(0, 0, 0))
 	# for line in arena:
 	# 	for rect in line:
 	# 		print(f'rect: {rect}; ', end='')
@@ -161,51 +118,57 @@ def draw_arena():
 	rect_x = arena[init_coords['X'][0]][init_coords['X'][1]]
 	rect_y = arena[init_coords['O'][0]][init_coords['O'][1]]
 	print(f'rect_x: {rect_x}; rect_y: {rect_y}')
-	rect_x.setFill(gr.color_rgb(255, 0, 0))
-	rect_y.setFill(gr.color_rgb(0, 0, 255))
-	for line in arena:
-		for rect in line:
-			print(f'rect: {rect}', end='')
-			rect.draw(win)
-		print()		
+	rect_y.setFill(RED)
+	rect_x.setFill(BLUE)
+	rect_y.draw(win)
+	rect_x.draw(win)
+	# for line in arena:
+	# 	for rect in line:
+	# 		print(f'rect: {rect}', end='')
+	# 		rect.draw(win)
+	# 	print()
+	label = draw_start_label(player)
+	win.getMouse()
+	label.undraw()	
 
-def draw_coords(coords, piece):
-	global player1_points, player2_points
-	y, x = coords[0], coords[1]
+
+def token_is_cor(piece, y, x):
 	y_incr = 0
 	for line in piece:
 		for i in range(len(line)):
 			if line[i] == '*':
 				rect = arena[(y + y_incr) % map_size_y][(x + i) % map_size_x]
-				rect.setFill(gr.color_rgb(255, 0, 0) if player == 'X' else gr.color_rgb(0, 0, 255))
-				rect.undraw()
-				rect.draw(win)
-				if player == PLAYER_1:
-					player1_points += 1
-				else:
-					player2_points += 1
+				if rect.canvas:
+					return True
 		y_incr += 1
+	return False
 
-def draw_quit_label(winner):
-	text = gr.Text(gr.Point(win.getWidth()/2 - 25, SCR_HEIGHT - Y_INDENT), 'Click on the screen to quit')
-	text.setTextColor(gr.color_rgb(255, 0, 0) if winner == PLAYER_1 else gr.color_rgb(0, 0, 255))
-	text.setSize(25)
-	text.setStyle('italic')
-	text.draw(win)
+def draw_coords(coords, piece):
+	y, x = coords[0], coords[1]
+	y_incr = 0
+	init_rect =  arena[y % map_size_y][x % map_size_x]
+	# init_rect =  arena[y][x]
+	if token_is_cor(piece, y, x):
+		for line in piece:
+			for i in range(len(line)):
+				if line[i] == '*':
+					rect = arena[(y + y_incr) % map_size_y][(x + i) % map_size_x]
+					# rect = arena[(y + y_incr)][(x + i)]
+					if not rect.canvas:
+						rect.setFill(RED if player == PLAYER_1 else BLUE)
+						rect.draw(win)
+			y_incr += 1
 
-def finish_the_game():
-	rect_up = make_rect(win.getWidth()/2 - 190, 20, win.getWidth()/2 + 170, Y_INDENT/2)
-	rect = make_rect(win.getWidth()/2 - 190, Y_INDENT/4, win.getWidth()/2 + 170, Y_INDENT - Y_INDENT/4)
-	rect.setOutline(gr.color_rgb(0, 0, 0))
-	rect.setWidth(LINE_WIDTH)
-	rect_up.setOutline(gr.color_rgb(0, 0, 0))
-	rect_up.setWidth(LINE_WIDTH)
-	winner = PLAYER_1 if player1_points >= player2_points else PLAYER_2
+def finish_the_game(player1_points, player2_points):
+
+	# rect_up = make_rect(win.getWidth()/2 - 190, 20, win.getWidth()/2 + 170, Y_INDENT/2)
+	rect = make_rect(win.getWidth()/2 - 230, INIT_Y/4, win.getWidth()/2 + 230, INIT_Y - INIT_Y/4, gr.color_rgb(0, 0, 0), LINE_WIDTH + 1)
+	winner = player1_name if player1_points >= player2_points else player2_name
 	max_points = max(player1_points, player2_points)
 	min_points = min(player1_points, player2_points)
-	text = gr.Text(gr.Point(win.getWidth()/2 - 15, Y_INDENT/2), f'Player {winner} won with {max_points} against {min_points}!')
-	rect.setFill(gr.color_rgb(255, 0, 0) if winner == PLAYER_1 else gr.color_rgb(0, 0, 255))
-	rect_up.setFill(gr.color_rgb(255, 0, 0) if winner == PLAYER_1 else gr.color_rgb(0, 0, 255))
+	print(f'Player {winner} is winner!')
+	text = gr.Text(gr.Point(win.getWidth()/2 - 2, INIT_Y/2), f'Player \'{winner}\' won with {max_points} against {min_points}!')
+	rect.setFill(RED if winner == player1_name else BLUE)
 	text.setTextColor(gr.color_rgb(0, 0, 0))
 	text.setSize(20)
 	rect.draw(win)
@@ -216,16 +179,43 @@ def finish_the_game():
 	win.close()
 	quit()
 
+def indent_and_scale():
+	global BOX_SIZE, INIT_X, INIT_Y
+	width = SCR_WIDTH - (INIT_X + Y_INDENT/2) * 2
+	height = SCR_HEIGHT - (INIT_Y + Y_INDENT/2) * 2
+	max_val = max(map_size_x, map_size_x)
+	max_dim = max(width, height)
+	if BOX_SIZE * max_val > max_dim:
+		while BOX_SIZE * max_val > max_dim:
+			BOX_SIZE -= 1
+	else:
+		while BOX_SIZE * max_val < max_dim:
+			BOX_SIZE += 1
+	indent = 0
+	wid_len = BOX_SIZE * map_size_x
+	end_x = SCR_WIDTH - INIT_X
+	while INIT_X + indent < end_x - wid_len - indent:
+		indent += 1
+		if indent > SCR_WIDTH/2:
+			error_and_quit(f'you stupid fuck!\n init_x: {INIT_X}, end_x: {end_x}, indent: {indent}, label_len: {label_len}')
+	INIT_X += indent
+	indent = 0
+	end_y = SCR_HEIGHT - INIT_Y
+	h_len = BOX_SIZE * map_size_y
+	while INIT_Y + indent < end_y - h_len - indent:
+		indent += 1
+		if indent > SCR_HEIGHT/2:
+			error_and_quit(f'you stupid fuck!\n init_x: {INIT_X}, end_x: {end_x}, indent: {indent}, label_len: {label_len}')
+	INIT_Y += indent		
 
 def display(coords, piece):
 	if not coords:
 		error_and_quit('no coords')
 
-	for line in piece:
-		print(line)		
-
-	if len(pl_map) == 0 or not pl_map:
+	if not piece:
 		finish_the_game()
+	# for line in piece:
+	# 	print(line)		
 	# print(f'len(pl_map): {len(pl_map)}')
 	# res_arena = []
 	# curr_y = coords[0]
@@ -254,4 +244,98 @@ def display(coords, piece):
 		draw_arena()
 	draw_coords(coords, piece)
 
-parse_map_and_display()
+def parse_map_and_display():
+	global map_size_x, map_size_y, piece_size_x, piece_size_y, player
+	# not_done_yet = True
+	for line in sys.stdin:
+		spl = line[:-1].split(' ')
+		# print(f'spl: {spl}, i: {i}')
+		if 'Plateau' in spl:
+			if int(spl[1]) != map_size_y or int(spl[2][:-1]) != map_size_x:
+				error_and_quit(f'new map_size({int(spl[1])}, {int(spl[2][:-1])}) is not equal to initial ({map_size_y}, {map_size_x})')
+			continue
+		elif re.match('^ +[0-9]+$', line) or re.match(f'^[0-9]+ .{{{map_size_x}}}$', line):
+			# ar_line = spl[1]
+			# if init_round and ('X' in ar_line or 'O' in ar_line):
+			# 	key = 'X' if 'X' in ar_line else 'O'
+			# 	init_coords[key][0] = arena_ind
+			# 	init_coords[key][1] = ar_line.index(key)
+			# pl_map.append(ar_line)
+			# arena_ind += 1
+			continue
+		elif 'Piece' in spl:
+			piece_size_y = int(spl[1])
+			piece_size_x = int(spl[2][:-1])
+			parse_piece(sys.stdin, piece_size_y)
+			continue
+		elif '<got' in spl:
+			coords = get_coords(spl)
+			player = spl[1][1]
+			display(coords, piece)
+			piece.clear()
+			print(f'curr_player: {player}')
+			if player != PLAYER_1 and player != PLAYER_2:
+				error_and_quit(f'your player\'s token is \'{player}\' and it isn\'t equal to \'{PLAYER_1}\' and \'{PLAYER_2}\'')
+			# not_done_yet = True
+			# time.sleep(3)
+			continue
+		elif spl[0] == '==' and (spl[1] == PLAYER_1 or spl[1] == PLAYER_2):
+			spl_end = sys.stdin.readline().split(' ')
+			finish_the_game(int(spl[-1]), int(spl_end[-1]))
+		else:
+			error_and_quit(f'Something\'s wrong in your input, guys. Here\'s the faulty line: {line}')
+	return
+
+def	init_parse_and_cycle():
+	global player, map_size_x, map_size_y, player1_name, player2_name, piece, arena
+	arena_ind = 0
+	for line in sys.stdin:
+		spl = line[:-1].split(' ')
+		print(f'spl: {spl}')
+		if 'p1' in spl or 'p2' in spl:
+			pl_name = spl[4].split('/')[-1][:-1]
+			if 'p1' in spl:
+				player1_name = pl_name
+			else:
+				player2_name = pl_name
+			continue
+		elif 'Plateau' in spl:
+			map_size_y = int(spl[1])
+			map_size_x = int(spl[2][:-1])
+			arena = [[gr.Rectangle(gr.Point(0, 0), gr.Point(0, 0))] * map_size_x for i in range(map_size_y)]
+			continue
+		elif re.match('^ +[0-9]+$', line):
+			continue
+		elif re.match(f'^[0-9]+ .{{{map_size_x}}}$', line):
+			ar_line = spl[1]
+			if 'X' in ar_line or 'O' in ar_line:
+				key = 'X' if 'X' in ar_line else 'O'
+				init_coords[key][0] = arena_ind
+				init_coords[key][1] = ar_line.index(key)
+			# pl_map.append(ar_line)
+			arena_ind += 1
+			continue
+		elif line[0] == '#' or 'launched' in spl:
+			continue
+		elif 'Piece' in spl:
+			piece_size_y = int(spl[1])
+			piece_size_x = int(spl[2][:-1])
+			parse_piece(sys.stdin, piece_size_y)
+			continue
+		elif '<got' in spl:
+			coords = get_coords(spl)
+			print(f'piece: {piece}')
+			player = spl[1][1]
+			indent_and_scale()
+			display(coords, piece)
+			# del pl_map
+			piece.clear()
+			parse_map_and_display()
+			# not_done_yet = True
+			# time.sleep(3)
+			continue
+		else:
+			error_and_quit(f'Something\'s wrong in your initial input, guys. Here\'s the faulty line: {line}')
+	return
+
+init_parse_and_cycle()
